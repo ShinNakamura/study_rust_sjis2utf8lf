013ee397c0a06d32c0f4811fb3416c0d8c38a2ee
ShinNakamura <kokoronopython@gmail.com>
2022-05-26 18:11:56 +0900

    main が Result 型を返すように変更

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,24 @@ use std::env;
 use std::fs;
 use std::io::{stdout, Write, BufWriter};
 
+type MyResult = Result<(), Box<dyn std::error::Error>>;
+
 // コマンドライン引数からファイルパスを1つだけ読み込んで
 // そのファイルが Shift_JIS である前提で(改行コードは切り捨てられる)
 // utf8 に変換し、改行コード LF (というか、プラットフォームの改行コード)で標準出力へ送る
-fn main() {
+fn main() -> MyResult {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1]; // ここは参照である必要がある。
     let out = stdout();
     // lock して排他処理を回避(スピードアップ目的)
     let mut out = BufWriter::new(out.lock());
-    let b = fs::read(filename).unwrap();
+    let b = fs::read(filename)?;
     let (res, _, _) = encoding_rs::SHIFT_JIS.decode(&b);
     let text = res.into_owned();
     // 行に分割してループする段階で元の改行コードを破棄
     for line in text.lines() {
         // writeln! を使用することでプラットフォームの改行コード＝LFで出力
-        writeln!(out, "{}", line).unwrap(); 
+        writeln!(out, "{}", line)?; 
     }
+    Ok(())
 }
